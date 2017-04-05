9d74697c1047acf159ec6b5c9971349e57cd74bd
sakmorme <sakmor@me.com>
2017-04-05 14:22:42 +0800

    新增玩家與怪物

--- Assets/main.cs
+++ Assets/main.cs
@@ -4,27 +4,51 @@ using System.Collections.Generic;
 public class main : MonoBehaviour
 {
     int[] cardsColor;
+    bool isCardCanTouch;
     GameObject[] cards;
     List<GameObject> openCardsList = new List<GameObject>();
     Vector2 gameSize;
     Camera cam;
     GameObject firstCard;
 
+    float emenyHP;
+    float playerHP;
+
     void Start()
     {
+        emenyHP = 300;
+        playerHP = 300;
+
         cam = GameObject.Find("Main Camera").GetComponent<Camera>();
         gameSize.x = 6;
         gameSize.y = 3;
         cards = GameObject.FindGameObjectsWithTag("card"); //將所有tag是cards的物件都記到cards
+        isCardCanTouch = false;
         randomCrads();  //洗牌
-        closeCardAfterTime(5);
-
+        hpUpdate_UI();
+        closeCardAfterTime(2.5f);
     }
 
     void Update()
     {
         mouseClickCard();
         contorl();
+    }
+    void hpUpdate_UI()
+    {
+        GameObject.Find("playerHP").GetComponent<UnityEngine.UI.Text>().text = "HP:" + playerHP;
+        GameObject.Find("enemyHP").GetComponent<UnityEngine.UI.Text>().text = "HP:" + emenyHP;
+
+    }
+    static void countDownTime(float n)
+    {
+        float catchTime = Time.time;
+
+    }
+
+    void shakeSprite(GameObject target)
+    {
+        target.GetComponent<Animator>().Play("hurt");
 
     }
 
@@ -37,30 +61,37 @@ public class main : MonoBehaviour
     }
     void closeCardAfterTime(float t)
     {
+
         Invoke("closeCard", t);
+    }
+    void setCardCanTouch(bool n)
+    {
+        isCardCanTouch = n;
     }
 
     void closeCard()
     {
 
-        for (int i = 0; i < 18; i++)
+        for (int i = 0; i < cardsColor.Length; i++)
         {
             //全部牌蓋起來
             cards[i].GetComponent<Renderer>().material =
-                (Material)UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/gray.mat", typeof(Material));
+                (Material)UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/Materials/gray.mat", typeof(Material));
         }
         //開啟被記錄在OpenCardsList的牌
         foreach (var n in openCardsList)
         {
             string temp = decodeColor(cardsColor[int.Parse(n.name)]);
-            n.transform.GetComponent<Renderer>().material = (Material)UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/" + temp + ".mat", typeof(Material));
+            n.transform.GetComponent<Renderer>().material = (Material)UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/Materials/" + temp + ".mat", typeof(Material));
         }
+        setCardCanTouch(true);
 
     }
 
     void mouseClickCard()
     {
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0)
+        && isCardCanTouch)
         {
             RaycastHit hit;
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -80,7 +111,7 @@ public class main : MonoBehaviour
         if (n.GetComponent<Renderer>().material.name == "gray (Instance)")
         {
             string temp = decodeColor(cardsColor[int.Parse(n.name)]);
-            n.transform.GetComponent<Renderer>().material = (Material)UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/" + temp + ".mat", typeof(Material));
+            n.transform.GetComponent<Renderer>().material = (Material)UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/Materials/" + temp + ".mat", typeof(Material));
             checkCard(n);
 
         }
@@ -91,16 +122,22 @@ public class main : MonoBehaviour
         {
             string nowCardColor = nowCard.GetComponent<Renderer>().material.name;
             string firstCardColor = firstCard.GetComponent<Renderer>().material.name;
+            setCardCanTouch(false);
             if (firstCardColor == nowCardColor)
             {
+                action(firstCardColor);
+                setCardCanTouch(true);
                 addKeepOpenCard(nowCard);
             }
             else
             {
+                attackPlayer();
+                GameObject.Find("cross").GetComponent<Animator>().Play("cross_jumpUp");
                 closeCardAfterTime(1);
-                resetFirstCard();
+
             }
-            firstCard = null;
+            resetFirstCard();
+
         }
         else
         {
@@ -108,6 +145,62 @@ public class main : MonoBehaviour
 
         }
     }
+    void attackPlayer()
+    {
+        playerHP -= 100;
+        GameObject.Find("player").GetComponent<Animator>().Play("hurtplayer");
+        GameObject.Find("emeny").GetComponent<Animator>().Play("attack");
+        hpUpdate_UI();
+    }
+    void action(string n)
+    {
+        switch (n)
+        {
+            case "red (Instance)":
+                attackEnemy();
+                break;
+            case "green (Instance)":
+                healPlay();
+                break;
+            case "blue (Instance)":
+                defensePlay();
+                break;
+            case "white (Instance)":
+                getExp();
+                break;
+        }
+
+    }
+    void attackEnemy()
+    {
+        emenyHP -= 100;
+        GameObject.Find("emeny").GetComponent<Animator>().Play("hurt");
+        GameObject.Find("player").GetComponent<Animator>().Play("attackplayer");
+
+        hpUpdate_UI();
+    }
+    void defensePlay()
+    {
+
+    }
+    void healPlay()
+    {
+
+    }
+    void getExp()
+    {
+
+    }
+    void emenySpeedup()
+    {
+
+    }
+    void emenyAttack()
+    {
+
+    }
+
+
     void resetFirstCard()
     {
         firstCard = null;
@@ -126,12 +219,12 @@ public class main : MonoBehaviour
     void randomCrads()
     {
         // 1代表紅色 2代表藍色 3代表綠色 4代表黃色 0代表白色
-        cardsColor = new int[] { 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 0, 0 };
+        cardsColor = new int[] { 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0 };
 
         //打散順序
-        for (int i = 0; i < 18; i++)
+        for (int i = 0; i < cardsColor.Length; i++)
         {
-            int r = Random.Range(0, 17);
+            int r = Random.Range(0, cardsColor.Length - 1);
             int temp = cardsColor[r];
             cardsColor[r] = cardsColor[i];
             cardsColor[i] = temp;
@@ -139,13 +232,13 @@ public class main : MonoBehaviour
 
         //放置顏色 
         string color = "white";
-        for (int i = 0; i < 18; i++)
+        for (int i = 0; i < cardsColor.Length; i++)
         {
 
             color = decodeColor(cardsColor[i]);
             cards[i].name = i.ToString();
             cards[i].GetComponent<Renderer>().material =
-                (Material)UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/" + color + ".mat", typeof(Material));
+                (Material)UnityEditor.AssetDatabase.LoadAssetAtPath("Assets/Materials/" + color + ".mat", typeof(Material));
         }
     }
 
